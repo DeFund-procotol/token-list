95085b28a4132c4c6173c269bd9cc5469a77c55d
rex <mailbaoer@gmail.com>
2022-11-15 11:51:26 +0800

    add icon export

--- src/__tests__/token-list.test.ts
+++ src/__tests__/token-list.test.ts
@@ -15,3 +15,7 @@ test('goerli tokens', () => {
 test('goerli ape token', () => {
   expect(new TokenList(5).token('APE').symbol === 'APE');
 });
+
+test('icon', () => {
+  expect(new TokenList(1).icon().length > 0);
+});
